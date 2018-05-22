28d414cc06e809fd904aa07f9ccee5f198b0c8a0
Olexandr Panov <futupas@gmail.com>
2018-05-22 21:01:39 +0300

    desktop: view1 all ready

--- view.js
+++ view.js
@@ -8,18 +8,26 @@ function AnimateCell(angle, cellnumber){
     }, 100);
 }
 
-var interval = setInterval(function(){
-    currentangle--;
-    AnimateCell(currentangle, 1);
-}, 8000);
+// var interval = setInterval(function(){
+//     currentangle--;
+//     AnimateCell(currentangle, 1);
+// }, 8000);
 
 document.body.onclick = function (e) {
     currentangle--;
     AnimateCell(currentangle, 1);
 
-    clearInterval(interval);
-    interval = setInterval(function(){
-        currentangle--;
-        AnimateCell(currentangle, 1);
-    }, 8000);
+    // clearInterval(interval);
+    // interval = setInterval(function(){
+    //     currentangle--;
+    //     AnimateCell(currentangle, 1);
+    // }, 8000);
+}
+
+var view1 = document.querySelector('.view.view1').cloneNode(true);
+view1.style.display = 'block';
+document.querySelector('.view.view1').remove();
+var sides1 = document.querySelectorAll('.cells > div > .side1');
+for (var i = 0; i < sides1.length; i++) {
+    sides1[i].appendChild(view1.cloneNode(true));
 }
